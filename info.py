a9adba68a10b4278f78c01413a16dc36111de33f
PREET-MEHRA-786 <135633167+PREET-MEHRA-786@users.noreply.github.com>
2023-09-21 22:22:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  =
-ADMIN        = 1255023013
-CHANNEL      = "BotszList"
+API_ID       = 25830285
+API_HASH     = "0cbcabbf1b9c56a4dfc4b8950d584a2a"
+BOT_TOKEN    = "6629504386:AAGkXB20WBzfnGKJ9B4BelUSt2UZQ2XS8TE"
+SESSION      = "BQGKI40ATCrfS73eMegygKL7J8Rn9ZJGoi6tIFq1eG7MbIyWLCfvFz41MLmh7m7DAmwCb3rimI3VjOeDdnOtjbKgbhhBZCBOub2ctKXMvXdx39_3OdFc43KtbsFBgkFUdbowV61MUqWGUVB-s0JSTC3rVGqijzXsYshrpJbEp7J8uP7dRaZYAr9QgE0jh6JeOSYumdDUbXkHHqWjC5RfqyrOEK7DNyM0hPa0eTVEuTK9QeMpYSxYYOZ0COihwrKdDx1PjuSkIe2Y6qpZ5oyFL_KJzy-hzE48nO7LYp6FGJDj2T_jbc7P5jHdnWkqVuRlv9eO_GkRYrodqLvwYdAx3SopkWjfmgAAAAFrQhqtAA"
+DATABASE_URI = "mongodb+srv://Searchbot:Mongodb.bot@cluster0.5xixyjh.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001961336094
+ADMIN        = 6651109872
+CHANNEL      = "movies_villa_backup"
